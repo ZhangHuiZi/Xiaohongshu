bb34af6c521742ed530f40627b15c56b961e71ea
张慧子 <June@zhzmacbook-pro.local>
2016-04-18 15:41:31 +0800

    初始化-创建项目

---
+++ Xiaohongshu/Xiaohongshu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Xiaohongshu
+//
+//  Created by 张慧子 on 16/4/18.
+//  Copyright © 2016年 张慧子. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Xiaohongshu/Xiaohongshu/main.m
@@ -0,0 +1,30 @@
+//
+//  main.m
+//  Xiaohongshu
+//
+//  Created by 张慧子 on 16/4/18.
+//  Copyright © 2016年 张慧子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+/*
+    必备技能:
+ 1. 根据设计文档对小红书iOS App产品的需求分析、详细设计，构架设计以及开发工作；
+ 2. 根据项目任务计划按时完成软件编码和单元测试工作；
+ 3. 解决研发过程中的关键问题和技术难题。
+        努力的我:
+ 1. 大学本科或以上学历，计算机相关专业，3年以上iOS相关经验
+ 2. 丰富的Objective-C经验，熟悉设计模式，能够设计灵活的iOS产品构架。熟悉C，C++
+ 3. 精通Cocoa/UIKit Framework，熟悉CoreAnimation，熟悉iPhone/iPad各个版本之间的差异，并且能够提供良好的解决方案
+ 4. 精通HTTP/JSON和XML等网络协议，熟悉多线程及面向对象编程，熟悉SQLite数据库编程
+ 5. 丰富的iOS性能优化经验
+ 6. 熟练使用Xcode的Instrument来诊断和调试问题
+ 7. 熟练编写Xcode下的单元测试。了解Xcode下的自动化测试
+ 8. 有Scrum敏捷开发经验优先
+ */
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
